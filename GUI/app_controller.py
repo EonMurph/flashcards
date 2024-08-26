b346596cf66792d8c88bf9326d6686fab4c36d1f
EonMurph <stiger.home@gmail.com>
2024-08-26 17:48:58 +0100

    Created method in controller class for running methods in model class that should be run upon loading the app

--- GUI/app_controller.py
+++ GUI/app_controller.py
@@ -17,8 +17,30 @@ class Flashcards:
         self._onLoad()
         self._connectSignalsAndSlots()
 
+    def _getFields(self, editors: list[FlashcardEditor]) -> dict[str, str]:
+        editorsText: list[str] = [
+            editor.textEditor.toPlainText() for editor in editors
+        ]
+        fields = {
+            field[0]: field[1]
+            for field in zip(
+                [
+                    field["name"]
+                    for field in self.data.currentFlashcard.model.fields
+                ],
+                editorsText,
+            )
+        }
+
+        return fields
     def _onLoad(self) -> None:
-        pass
+        """Method to be called upon loading the window. This method will call some starter functions in `self.model` so that the loaded window will display properly."""
+        for editor in self.view.editors:
+            self.model.TextOperations.renderPreview(
+                fields=self._getFields(editors=self.view.editors),
+                previews=self.view.flashcardPreviews,
+                template=self.data.currentFlashcard.model.templates[0]
+            )
 
     def _connectSignalsAndSlots(self) -> None:
         self.view.noteCreator.clicked.connect(
@@ -28,27 +50,11 @@ class Flashcards:
             lambda: self.model.FlashcardOperations.deleteFlashcard()
         )
 
-        def getFields(editors: list[FlashcardEditor]) -> dict[str, str]:
-            editorsText: list[str] = [
-                editor.textEditor.toPlainText() for editor in editors
-            ]
-            fields = {
-                field[0]: field[1]
-                for field in zip(
-                    [
-                        field["name"]
-                        for field in self.data.currentFlashcard.model.fields
-                    ],
-                    editorsText,
-                )
-            }
-
-            return fields
 
         for editor in self.view.editors:
             editor.textEditor.textChanged.connect(
                 lambda e=editor: self.model.TextOperations.renderPreview(
-                    fields=getFields(editors=self.view.editors),
+                    fields=self._getFields(editors=self.view.editors),
                     previews=self.view.flashcardPreviews,
                     template=self.data.currentFlashcard.model.templates[0],
                 )
